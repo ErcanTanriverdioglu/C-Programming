4b941ecf270e91adaa1e5f3a8d0f805a01b6d734
Ercan Tanrıverdioğlu <122203202+ErcanTanriverdioglu@users.noreply.github.com>
2023-12-28 13:34:50 +0300

    15_FunctionPointerCalculator.c

--- 15_FunctionPointerCalculator.c
+++ 15_FunctionPointerCalculator.c
@@ -4,31 +4,25 @@
 #include <ctype.h>
 
 float topla(float x, float y)
-{
-    return x + y;
+{  return x + y;
 }
 float cikar(float x, float y)
-{
-    return x - y;
+{   return x - y;
 }
 float carp(float x, float y)
-{
-    return x * y;
+{  return x * y;
 }
 float bol(float x, float y)
-{
-    return x / y;
+{ return x / y;
 }
 
 int main()
 {
     while (1) {
-
         float x = 0;
         float y = 0;
         char islem[20];
         char kontrol;
-
         float (*fonksiyon)(float, float);
        
         printf("Isleme devam etmek icin herhangi bir tusa basiniz.\nCikmak icin ise x'e basiniz: ");
@@ -41,29 +35,18 @@ int main()
         scanf("%s", islem);
 
         if ((strcmp(islem, "topla") == 0))
-        {
-            fonksiyon = topla;
-        }
+                 fonksiyon = topla;
         if ((strcmp(islem, "cikar") == 0))
-        {
-            fonksiyon = cikar;
-        }
+                 fonksiyon = cikar;
         if ((strcmp(islem, "carp") == 0))
-        {
-            fonksiyon = carp;
-        }
+                 fonksiyon = carp;
         if ((strcmp(islem, "bol") == 0))
-        {
-            fonksiyon = bol;
-        }
+                 fonksiyon = bol;
         if ((strcmp(islem, "bol") != 0) && (strcmp(islem, "carp") != 0) && (strcmp(islem, "topla") != 0) && (strcmp(islem, "cikar") != 0))
-        {
-            fprintf(stderr, "GIRILEN ISLEM BULUNAMADI.....\n");
-            abort();
+        { fprintf(stderr, "GIRILEN ISLEM BULUNAMADI.....\n");
+          abort();
         }
-        printf("Islem sonucu = %.2f dir. \n", fonksiyon(x, y));
-
-        
+        printf("Islem sonucu = %.2f dir. \n", fonksiyon(x, y));   
     }
     return EXIT_SUCCESS;
 }
